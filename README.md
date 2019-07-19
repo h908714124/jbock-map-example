5738f4f39d128644780dad30a71dfae99c966363
h908714124 <kraftdurchblumen@gmx.de>
2019-07-20 01:10:41 +0200

    fix source compat

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ gradle clean run --args='--help'
 gradle -Duser.language=es clean run --args='--help'
 ````
 
-Or compile to native with [graal](https://www.graalvm.org/):
+Or [compile to native:](https://github.com/oracle/graal/releases)
 
 ````sh
-gradle clean jar
-$GRAALVM_HOME/bin/native-image -H:IncludeResourceBundles=de.h90.example.bundle.MyBundle -jar build/libs/jbock-map-example.jar map_example
+gradle jar
+native-image -H:IncludeResourceBundles=de.h90.example.bundle.MyBundle -jar build/libs/jbock-map-example.jar map_example
 ./map_example -Xhorse:12 -Xsheep:4
 ./map_example --help
 ````
--- src/main/java/de/h90/example/MapArgs.java
+++ src/main/java/de/h90/example/MapArgs.java
@@ -22,26 +22,28 @@ abstract class MapArgs {
      * A HTTP header
      */
     @Parameter(repeatable = true,
-            shortName = 'X',
+            shortName = 'H',
             mappedBy = MapTokenizer.class,
             collectedBy = MapCollector.class,
             bundleKey = "headers")
     abstract Map<String, String> headers();
 
+    // sample mapper
     static class MapTokenizer implements Supplier<Function<String, Map.Entry<String, String>>> {
 
         @Override
         public Function<String, Map.Entry<String, String>> get() {
             return s -> {
-                String[] tokens = s.split(":", 2);
-                if (tokens.length < 2) {
-                    throw new IllegalArgumentException("Invalid pair: " + s);
+                String[] tokens = s.split("[=]", 2);
+                if (tokens.length != 2) {
+                    throw new IllegalArgumentException("try '-X Header1=foo -X Header2=bar'");
                 }
                 return new AbstractMap.SimpleImmutableEntry<>(tokens[0].trim(), tokens[1].trim());
             };
         }
     }
 
+    // sample collector
     static class MapCollector<K, V> implements Supplier<Collector<Map.Entry<K, V>, ?, Map<K, V>>> {
 
         @Override
@@ -50,3 +52,4 @@ abstract class MapArgs {
         }
     }
 }
+
