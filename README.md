fd8f2fa00410c6c6d21a78b4a0dad9e7a6bef59a
h908714124 <kraftdurchblumen@gmx.de>
2019-07-20 19:35:29 +0200

    use shadow

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 Collecting parameters into a key-value map with [jbock.](https://github.com/h908714124/jbock)
 
 ````sh
-gradle clean run --args='-Xhorse:12 -Xsheep:4'
+gradle clean run --args='-Xhorse=12 -Xsheep=4'
 gradle clean run --args='--help'
 gradle -Duser.language=es clean run --args='--help'
 ````
 
-Or [compile to native:](https://github.com/oracle/graal/releases)
+Or [compile to native](https://github.com/oracle/graal/releases) first:
 
 ````sh
-gradle jar
-native-image -H:IncludeResourceBundles=de.h90.example.bundle.MyBundle -jar build/libs/jbock-map-example.jar map_example
-./map_example -Xhorse:12 -Xsheep:4
+gradle shadowJar
+native-image --no-fallback -H:IncludeResourceBundles=de.h90.example.bundle.MyBundle -jar build/libs/jbock-map-example-all.jar map_example
+./map_example -Xhorse=12 -Xsheep=4
 ./map_example --help
 ````
 
--- src/main/java/de/h90/example/MapArgs.java
+++ src/main/java/de/h90/example/MapArgs.java
@@ -2,9 +2,11 @@ package de.h90.example;
 
 import net.jbock.CommandLineArguments;
 import net.jbock.Parameter;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
@@ -14,41 +16,46 @@ import java.util.stream.Collectors;
  * This is the program description. Its internationalization bundle key is 'jbock.description'.
  */
 @CommandLineArguments(
-    programName = "parse-map",
-    missionStatement = "This is the project's mission. Its internationalization bundle key is 'jbock.mission'.")
+        programName = "parse-map",
+        missionStatement = "This is the project's mission. Its internationalization bundle key is 'jbock.mission'.")
 abstract class MapArgs {
 
     /**
      * A HTTP header
      */
-    @Parameter(repeatable = true,
-            shortName = 'H',
+    @Parameter(
+            repeatable = true,
+            shortName = 'X',
             mappedBy = MapTokenizer.class,
             collectedBy = MapCollector.class,
             bundleKey = "headers")
     abstract Map<String, String> headers();
 
     // sample mapper
-    static class MapTokenizer implements Supplier<Function<String, Map.Entry<String, String>>> {
+    static class MapTokenizer implements Supplier<Function<String, Entry<String, String>>> {
 
         @Override
-        public Function<String, Map.Entry<String, String>> get() {
-            return s -> {
-                String[] tokens = s.split("[=]", 2);
-                if (tokens.length != 2) {
-                    throw new IllegalArgumentException("try '-X Header1=foo -X Header2=bar'");
-                }
-                return new AbstractMap.SimpleImmutableEntry<>(tokens[0].trim(), tokens[1].trim());
-            };
+        public Function<String, Entry<String, String>> get() {
+            return this::splitToken;
+        }
+
+        Entry<String, String> splitToken(String s) {
+            String[] tokens = s.split("[=]", 2);
+            if (tokens.length != 2) {
+                throw new IllegalArgumentException("try '-X Header1=foo -X Header2=bar'");
+            }
+            String k = StringUtils.trim(tokens[0]);
+            String v = StringUtils.trim(tokens[1]);
+            return new SimpleImmutableEntry<>(k, v);
         }
     }
 
     // sample collector
-    static class MapCollector<K, V> implements Supplier<Collector<Map.Entry<K, V>, ?, Map<K, V>>> {
+    static class MapCollector<K, V> implements Supplier<Collector<Entry<K, V>, ?, Map<K, V>>> {
 
         @Override
-        public Collector<Map.Entry<K, V>, ?, Map<K, V>> get() {
-            return Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue);
+        public Collector<Entry<K, V>, ?, Map<K, V>> get() {
+            return Collectors.toMap(Entry::getKey, Entry::getValue);
         }
     }
 }
